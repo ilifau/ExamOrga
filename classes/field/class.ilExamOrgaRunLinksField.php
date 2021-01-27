248d385455b6b9593823cde0b36fb64b926195d9
Fred Neumann <fred.neumann@fau.de>
2021-01-27 11:13:18 +0100

    show links in list

--- classes/field/class.ilExamOrgaRunLinksField.php
+++ classes/field/class.ilExamOrgaRunLinksField.php
@@ -7,10 +7,8 @@ class ilExamOrgaRunLinksField extends ilExamOrgaField
      * @inheritdoc
      */
     public function getListHTML($record) {
-        // may be a too long list
-        // query record by record is slow
-        // so isForlist() is yet set to false
-        return $this->getDetailsHTML($record);
+        require_once (__DIR__ . '/../links/class.ilExamOrgaLink.php');
+        return ilExamOrgaLink::getRecordLinksHtml($record->id);
     }
 
     /**
@@ -79,13 +77,5 @@ class ilExamOrgaRunLinksField extends ilExamOrgaField
      */
     public function setExcelValue($record, $excel, $value) {
         // nothing to set
-    }
-
-    /**
-     * Check if the field can be used in a list of records
-     * @return bool
-     */
-    public function isForList() {
-        return false;
     }
 }
--- classes/links/class.ilExamOrgaLink.php
+++ classes/links/class.ilExamOrgaLink.php
@@ -124,4 +124,21 @@ class ilExamOrgaLink extends ActiveRecord
 
         return implode("\n", $entries);
     }
+
+    /**
+     * Get a textual representation of the links
+     */
+    public static function getRecordLinksHtml($record_id)
+    {
+        /** @var self[] $links */
+        $links = self::where(['record_id' => $record_id])->orderBy('exam_run')->get();
+
+        $entries = [];
+        foreach ($links as $link) {
+            $entries[] = $link->exam_run . ' &nbsp; <a target="_blank" href="' . $link->link . '">' . $link->link . '</a>';
+        }
+
+        return implode("<br />", $entries);
+    }
+
 }

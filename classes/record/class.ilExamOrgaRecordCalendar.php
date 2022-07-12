0d4380671165cd402d673967ebc3b3985fa75760
Christina Fuchs <christina.fuchs@ili.fau.de>
2022-07-12 18:40:27 +0200

    refactoring to remove class ilICalWriter

--- classes/record/class.ilExamOrgaRecordCalendar.php
+++ classes/record/class.ilExamOrgaRecordCalendar.php
@@ -8,6 +8,12 @@ include_once './Services/Calendar/classes/iCal/class.ilICalWriter.php';
  */
 class ilExamOrgaRecordCalendar
 {
+    const LINEBREAK = "\r\n";
+    #const LINEBREAK = '<br />';
+    // minus one to fix multi line breaks.
+    const LINE_SIZE = 74;
+    const BEGIN_LINE_WHITESPACE = ' ';
+
     /** @var ilObjExamOrga */
     protected $object;
 
@@ -23,8 +29,8 @@ class ilExamOrgaRecordCalendar
     /** @var string */
     protected $error;
 
-    /** @var ilICalWriter */
-    protected $writer;
+    /** @var string */
+    private $ical = '';
     
     /**
      * Initialize the data
@@ -34,7 +40,7 @@ class ilExamOrgaRecordCalendar
     {
         $this->object = $object;
         $this->plugin = $object->plugin;
-        $this->writer = new ilICalWriter();
+        $this->ical = '';
 
         $fields = $object->getAvailableFields();
 
@@ -84,10 +90,10 @@ class ilExamOrgaRecordCalendar
         $fields = $this->fields;
         $records = $this->records;
 
-        $this->writer->addLine('BEGIN:VCALENDAR');
-        $this->writer->addLine('VERSION:2.0');
-        $this->writer->addLine('METHOD:PUBLISH');
-        $this->writer->addLine('PRODID:-//ilias.de/NONSGML ILIAS Calendar V4.4//EN');
+        $this->addLine('BEGIN:VCALENDAR');
+        $this->addLine('VERSION:2.0');
+        $this->addLine('METHOD:PUBLISH');
+        $this->addLine('PRODID:-//ilias.de/NONSGML ILIAS Calendar V4.4//EN');
       //  $this->addTimezone(); TODO
         
         $i = 0;
@@ -107,25 +113,75 @@ class ilExamOrgaRecordCalendar
             // TODO: calculate category 
 
             // example event; TODO: write records from Exam Orga
-            $this->writer->addLine('BEGIN:VEVENT');
-            $this->writer->addLine('SUMMARY:'.$record->fau_unit.'-'.$record->exam_title);
-            $this->writer->addLine('UID:studon-c7614cff-3549-4a00-9152-d25cc1fe077d'.$i); // TODO UID generieren
-            $this->writer->addLine('SEQUENCE:0'); // TODO: implement more than 1 runs
-            $this->writer->addLine('LOCATION:'.$record->room);
-            $this->writer->addLine('DTSTART;Europe/Berlin:'.$examStart);
-            $this->writer->addLine('DTEND;Europe/Berlin:'.$examEnd);
-            $this->writer->addLine('DTSTAMP;;Europe/Berlin:'.$updatedTime);
-            $this->writer->addLine('CATEGORIES:Gelbe Kategorie');
-            $this->writer->addLine('DESCRIPTION:Dozierender: Max Mustermann3\nE-Mail: Max.Mustermann@fautest.de\nHiwis & Azubis:\nTeilnehmende: 10\nSelbstregistrieungscode: fgRtz%69\nLink: https://studontest.fautest.de');
-            $this->writer->addLine('END:VEVENT');
+            $this->addLine('BEGIN:VEVENT');
+            $this->addLine('SUMMARY:'.$record->fau_unit.'-'.$record->exam_title);
+            $this->addLine('UID:studon-c7614cff-3549-4a00-9152-d25cc1fe077d'.$i); // TODO UID generieren
+            $this->addLine('SEQUENCE:0'); // TODO: implement more than 1 runs
+            $this->addLine('LOCATION:'.$record->room);
+            $this->addLine('DTSTART;Europe/Berlin:'.$examStart);
+            $this->addLine('DTEND;Europe/Berlin:'.$examEnd);
+            $this->addLine('DTSTAMP;;Europe/Berlin:'.$updatedTime);
+            $this->addLine('CATEGORIES:Gelbe Kategorie');
+            $this->addLine('DESCRIPTION:Dozierender: Max Mustermann3\nE-Mail: Max.Mustermann@fautest.de\nHiwis & Azubis:\nTeilnehmende: 10\nSelbstregistrieungscode: fgRtz%69\nLink: https://studontest.fautest.de');
+            $this->addLine('END:VEVENT');
             $i++;
         }
 
-        $this->writer->addLine('END:VCALENDAR');
+        $this->addLine('END:VCALENDAR');
     }
 
     public function getExportString()
     {
-        return $this->writer->__toString();
+        return $this->ical;
+    }
+
+  
+    protected static function escapeText($a_text)
+    {
+        $a_text = str_replace("\r\n", '\\n', $a_text);
+
+        return preg_replace(
+            array(
+                '/\\\/',
+                '/;/',
+                '/,/',
+                ),
+            array(
+                '\\',
+                '\;',
+                '\,',
+                ),
+            $a_text
+            );
+    }
+    
+    /**
+     * Add a line to the ical string
+     * @return
+     * @param object $a_line
+     */
+    protected function addLine($a_line)
+    {
+        //$chunks = str_split($a_line, self::LINE_SIZE);
+
+        include_once './Services/Utilities/classes/class.ilStr.php';
+
+        // use multibyte split
+        $chunks = array();
+        $len = ilStr::strLen($a_line);
+        while ($len) {
+            $chunks[] = ilStr::subStr($a_line, 0, self::LINE_SIZE);
+            $a_line = ilStr::subStr($a_line, self::LINE_SIZE, $len);
+            $len = ilStr::strLen($a_line);
+        }
+
+        for ($i = 0; $i < count($chunks); $i++) {
+            $this->ical .= $chunks[$i];
+            if (isset($chunks[$i + 1])) {
+                $this->ical .= self::LINEBREAK;
+                $this->ical .= self::BEGIN_LINE_WHITESPACE;
+            }
+        }
+        $this->ical .= self::LINEBREAK;
     }
 }

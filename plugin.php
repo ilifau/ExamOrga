045a428de0e6329932b612bf034c1fc671f297f3
Christina Fuchs <chr.fuchs@fau.de>
2024-07-16 14:30:09 +0200

    increase version to ilias 9

--- plugin.php
+++ plugin.php
@@ -4,12 +4,12 @@
 $id = 'xamo';
  
 // code version; must be changed for all code changes
-$version = '0.1.2';
+$version = '0.1.3';
  
 // ilias min and max version; must always reflect the versions that should
 // run with the plugin
-$ilias_min_version = '7.0';
-$ilias_max_version = '7.999';
+$ilias_min_version = '9.0';
+$ilias_max_version = '9.999';
  
 // optional, but useful: Add one or more responsible persons and a contact email
 $responsible = 'Fred Neumann';

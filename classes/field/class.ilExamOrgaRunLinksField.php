63a006f62e62a686de8620e3dfee2cbaaae77f52
Christina Fuchs <chr.fuchs@fau.de>
2024-09-20 14:58:52 +0200

    update to ilias 9 - some fixes

--- classes/field/class.ilExamOrgaRunLinksField.php
+++ classes/field/class.ilExamOrgaRunLinksField.php
@@ -31,8 +31,8 @@ class ilExamOrgaRunLinksField extends ilExamOrgaField
      * @inheritdoc
      */
     public function getListHTML($record) {
-        require_once (__DIR__ . '/../links/class.ilExamOrgaLink.php');
-        return ilExamOrgaLink::getRecordLinksHtml($record->getId(), $this->links[$record->getId()]);
+        $links = isset($this->links) ? $this->links[$record->getId()] : null;
+        return ilExamOrgaLink::getRecordLinksHtml($record->getId(), $links);
     }
 
     /**

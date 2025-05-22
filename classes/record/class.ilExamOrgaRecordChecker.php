4d79653e5e8717d97e16aedf72bf03de4178222a
chrfau <chr.fuchs@fau.de>
2025-05-22 13:14:19 +0200

    undo remove fields from exam orga object form - admins, correctors and monitors

--- classes/record/class.ilExamOrgaRecordChecker.php
+++ classes/record/class.ilExamOrgaRecordChecker.php
@@ -161,7 +161,7 @@ class ilExamOrgaRecordChecker
         // notes should only be updated when a record is interactively  created or updated
         if ($this->purpose == self::PURPOSE_SAVE) {
             $this->updateNotes(ilExamOrgaNote::TYPE_CAMPUS, $this->warnings[ilExamOrgaMessage::TYPE_WARNING_CAMPUS]);
-           // $this->updateNotes(ilExamOrgaNote::TYPE_ROLES, $this->warnings[ilExamOrgaMessage::TYPE_WARNING_ROLES]);
+            $this->updateNotes(ilExamOrgaNote::TYPE_ROLES, $this->warnings[ilExamOrgaMessage::TYPE_WARNING_ROLES]);
             $this->updateNotes(ilExamOrgaNote::TYPE_CONDITION, $this->warnings[ilExamOrgaMessage::TYPE_WARNING_CONDITION]);
         }
 
